44e8c6ceb07911f9b8882454ee756a3fff0238a7
Christian Heimlich <chris@pcserenity.com>
2022-12-27 13:29:06 -0500

    Allow specifying Qx components in FetchQx

--- cmake/module/OB/FetchQx.cmake
+++ cmake/module/OB/FetchQx.cmake
@@ -2,12 +2,46 @@
 
 # git_ref - Tag, branch name, or commit hash to retrieve. According to CMake docs,
 #           a commit hash is preferred for speed and reliability
+# components - An optional semi-colon or whitespace delimited list of Qx components.
+#              If provided, only the components in the list will be configured.
 
-function(fetch_qx git_ref)
+function(fetch_qx)
+    # Additional Function inputs
+    set(oneValueArgs
+        REF
+    )
+    set(multiValueArgs
+        COMPONENTS
+    )
+
+    # Parse arguments
+    cmake_parse_arguments(FETCH_QX "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    # Validate input
+    foreach(unk_val ${FETCH_QX_UNPARSED_ARGUMENTS})
+        message(WARNING "Ignoring unrecognized parameter: ${unk_val}")
+    endforeach()
+
+    if(${FETCH_QX_KEYWORDS_MISSING_VALUES})
+        foreach(missing_val ${FETCH_QX_KEYWORDS_MISSING_VALUES})
+            message(ERROR "A value for '${missing_val}' must be provided")
+        endforeach()
+        message(FATAL_ERROR "Not all required values were present!")
+    endif()
+
+    if(FETCH_QX_REF)
+        set(OPTIONAL_REF "GIT_TAG" ${FETCH_QX_REF})
+    endif()
+
+    if(FETCH_QX_COMPONENTS)
+        set(QX_COMPONENTS ${FETCH_QX_COMPONENTS})
+    endif()
+    
+
     include(FetchContent)
     FetchContent_Declare(Qx
         GIT_REPOSITORY "https://github.com/oblivioncth/Qx"
-        GIT_TAG ${git_ref}
+        ${OPTIONAL_REF}
     )
     FetchContent_MakeAvailable(Qx)
 endfunction()
