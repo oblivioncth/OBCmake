eaccb51abbf064d2bf7012dbff58413d8a328988
Christian Heimlich <chris@pcserenity.com>
2023-03-02 15:21:08 -0500

    Add linkage to output package name, based on BUILD_SHARED_LIBS

--- cmake/module/OB/Packaging.cmake
+++ cmake/module/OB/Packaging.cmake
@@ -4,6 +4,8 @@
 # Assumes README.md and LICENSE are in root project directory, places package out in
 # ${CMAKE_CURRENT_BINARY_DIR}/out/dist by default.
 #
+# Makes use of BUILD_SHARED_LIBS to determine if build should be marked as Static or Shared.
+#
 # The suffix argument does not automatically use a leading "_"
 #
 # Requires ob_standard_project_setup() or ob_setup_verbose_versioning() otherwise
@@ -42,10 +44,17 @@ function(ob_standard_project_package)
     # Get system architecture
     include(OB/Utility)
     ob_get_system_architecture(__target_arch)
+    
+    # Determine linkage string
+    if(BUILD_SHARED_LIBS)
+        set(link_str "Shared")
+    else()
+        set(link_str "Static")
+    endif()
 
     set(CPACK_PACKAGE_VENDOR "${STD_PKG_VENDOR}")
     set(CPACK_PACKAGE_DIRECTORY "${__output_pkg_dir}")
-    set(CPACK_PACKAGE_FILE_NAME "${PROJECT_NAME}_${PROJECT_VERSION_VERBOSE}_${CMAKE_SYSTEM_NAME}_${__target_arch}${STD_PKG_SUFFIX}")
+    set(CPACK_PACKAGE_FILE_NAME "${PROJECT_NAME}_${PROJECT_VERSION_VERBOSE}_${CMAKE_SYSTEM_NAME}_${link_str}_${__target_arch}${STD_PKG_SUFFIX}")
     set(CPACK_GENERATOR "ZIP")
     set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
     set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
