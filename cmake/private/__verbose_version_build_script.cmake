479ea877e6f612cbe16fa0fde836cb499eefae97
Christian Heimlich <chris@pcserenity.com>
2024-10-09 02:46:31 -0400

    Simplify verbose version setup
    Only need to find Git once per project tree, hopefully slightly speeds
    this up.

--- cmake/private/__verbose_version_build_script.cmake
+++ cmake/private/__verbose_version_build_script.cmake
@@ -1,3 +1,11 @@
+# Variables Available to this Script:
+#
+# GIT_EXECUTEABLE
+# GIT_REPO_DIR
+# VERSION_GET_FILE
+# VERBOSE_VER_CACHE
+# PROJECT_NAME
+
 # Get cached verbose version from disk
 if(EXISTS "${VERBOSE_VER_CACHE}")
     file(READ ${VERBOSE_VER_CACHE} CACHED_VERBOSE_VER)
@@ -6,11 +14,11 @@ else()
 endif()
 
 # Get fresh verbose version
-include("${__OB_CMAKE_PRIVATE}/__verbose_version_get_version.cmake")
-__ob_get_verbose_version("${GIT_REPO}" "${VERSION_FALLBACK}" VERBOSE_VER)
+include(${VERSION_GET_FILE})
+__ob_get_verbose_version("${GIT_REPO_DIR}" VERBOSE_VER)
 
 # Compare values, and update if necessary
-if(NOT ("${CACHED_VERBOSE_VER}" STREQUAL "${VERBOSE_VER}"))
+if(NOT "${CACHED_VERBOSE_VER}" STREQUAL "${VERBOSE_VER}")
     message(STATUS "${PROJECT_NAME} Verbose version is out of date")
     # This will update verbose_ver.txt, causing cmake to reconfigure
     file(WRITE ${VERBOSE_VER_CACHE} ${VERBOSE_VER})
